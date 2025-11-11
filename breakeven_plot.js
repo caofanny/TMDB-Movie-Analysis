0c5493b3df248349934340cfcd7e51e6db76a8f3
caofanny <cao.fa@northeastern.edu>
2025-11-11 00:33:11 -0500

    Created plot to look at breakeven, profit, and loss

--- breakeven_plot.js
+++ breakeven_plot.js
@@ -3,37 +3,90 @@
 //       profitability depends on how many more times it is earning 
 //   selection for the different genres (only take in the first genre)
 
-let 
-    width = 500,
-    height = 600;
+const movie_data = d3.csv('movies_cleaned.csv')
 
-let margin = { 
-    top: 50,
-    bottom: 50,
-    left: 50,
-    right: 50
-};
+// loads the data and changes budget and revenue into numbers
+movie_data.then(function(data) {
+    
+    data.forEach(function(d) {
+        d.budget = +d.budget;
+        d.revenue = +d.revenue; 
+        // calculate the ratio between revenue and budget
+        d.profitability = d.revenue / d.budget;
 
-let svg = d3.select('body')
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height)
-            .style('background', 'gray')
+        // removes the ' and []
+        let str_genre = d.genres.replace(/[\[\]']/g, '');
+        // gets the forst genre from the list
+        d.genres = str_genre.split(',')[0].trim();
+    });
 
-// define the scales
-let yScale = d3.scaleLinear() // linear bc it is a linear relationship for the domain
-              .domain([0,10]) // domain is rata range you want to represent
-              .range([height - margin.bottom, margin.top])
+    function assign_colors(profitability){
+        if (profitability < 1) return 'red';    // loss
+        if (profitability < 2) return 'yellow'; // break-even
+        if (profitability < 5) return 'blue';   // profitable
+        return 'green';                         // extremely profitable`    
+    }
 
-let xScale = d3.scaleBand() // band bc x-axis might be divided into different bins
-              .domain(data.map(d => d.name)) // d: data, d.name: data in each col
-              .range([margin.left, width - margin.right])
+    // defining the svg margins and dimensions
+    let 
+        width = 800,
+        height = 600;
 
-// make axis with the scale functions
-let xAxis = svg.append('g')
-              .call(d3.axisBottom().scale(xScale))
-              .attr('transform', `translate(0, ${height - margin.bottom})`)
+    let margin = { 
+        top: 50,
+        bottom: 50,
+        left: 100,
+        right: 50
+    };
 
-let yAxis = svg.append('g')
-              .call(d3.axisLeft().scale(yScale))
-              .attr('transform', `translate(${margin.left}, 0)`)
+    // create the svg container
+    let svg = d3.select('#breakeven')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height)
+                .style('background', 'white');
+
+    // define the scales
+    let yScale = d3.scaleLinear() // linear bc it is a linear relationship for the domain
+                .domain([d3.min(data, d => d.revenue), d3.max(data, d => d.revenue)]) // domain is rata range you want to represent
+                .range([height - margin.bottom, margin.top]);
+
+    let xScale = d3.scaleLinear()
+                .domain([d3.min(data, d => d.budget), d3.max(data, d => d.budget)]) 
+                .range([margin.left, width - margin.right]);
+
+    // make axis with the scale functions
+    let xAxis = svg.append('g')
+                .call(d3.axisBottom().scale(xScale))
+                .attr('transform', `translate(0, ${height - margin.bottom})`);
+
+    let yAxis = svg.append('g')
+                .call(d3.axisLeft().scale(yScale))
+                .attr('transform', `translate(${margin.left}, 0)`);
+
+    // plot points
+    let circle = svg.selectAll('circle')
+                    .data(data)
+                    .enter()
+                    .append('circle')
+                    .attr('r', 3)
+                    .attr('cx', d => xScale(d.budget))
+                    .attr('cy', d => yScale(d.revenue))
+                    .attr('opacity', 0.6)
+                    .attr('fill', d => assign_colors(d.profitability));
+
+    // add x-axis label              
+    svg.append('text')
+        .attr('x', width/2)
+        .attr('y', height - 15)
+        .text('Budget')
+        .style('text-anchor', 'middle');
+
+    // add y-axis label
+    svg.append('text')
+        .attr('x', 0 - height/2)
+        .attr('y', 15)
+        .text('Revenue')
+        .style('text-anchor', 'middle')
+        .attr('transform', 'rotate(-90)');
+});
